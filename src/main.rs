055d514a4eaeb67c0fbcb259a14c410691e8c82f
gradiuscypher@gmail.com <jared.rathbun@discordapp.com>
2021-09-23 16:22:41 -0700

    I understand a few more things

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
 use std::{env, process::Command};
 
-use serenity::{async_trait, futures::io::Read, model::{
-        gateway::Ready,
-        id::GuildId,
-        interactions::{
-            application_command::{
-                ApplicationCommand,
-                ApplicationCommandInteractionDataOptionValue,
-                ApplicationCommandOptionType
-            },
-            Interaction,
-            InteractionResponseType
-        },
-    }, prelude::*};
+use serenity::{async_trait, futures::io::Read, model::{gateway::Ready, id::GuildId, interactions::{Interaction, InteractionResponseType, message_component::{ButtonStyle}, application_command::{ApplicationCommand, ApplicationCommandInteractionDataOptionValue, ApplicationCommandOptionChoice, ApplicationCommandOptionType}}}, prelude::*};
 
 struct Handler;
 
@@ -20,93 +8,88 @@ struct Handler;
 impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
         if let Interaction::ApplicationCommand(command) = interaction {
-            let content = match command.data.name.as_str() {
-                "ping" => "Hey I'm alive!".to_string(),
-                "id" => {
-                    let options = command
-                        .data
-                        .options
-                        .get(0)
-                        .expect("Expected user option")
-                        .resolved
-                        .as_ref()
-                        .expect("Expected user object");
-                    
-                    if let ApplicationCommandInteractionDataOptionValue::User(user, _member) = 
-                        options
+            match command.data.name.as_str() {
+                "gcommand1" => {
+                    if let Err(why) = command.create_interaction_response(&ctx.http, |response| {
+                        response
+                            .kind(InteractionResponseType::ChannelMessageWithSource)
+                            .interaction_response_data(|message| {
+                                message.content("I've run the gcommand!".to_string());
+                                message.components(|c| {
+                                    c.create_action_row(|ar| {
+                                        ar.create_button(|b| {
+                                            b.style(ButtonStyle::Primary);
+                                            b.label("Test Button");
+                                            b.custom_id("test_button");
+                                            b
+                                        })
+                                    })
+                                })
+                            })
+                    })
+                    .await
                     {
-                        format!("{}'s ID is {}", user.tag(), user.id)
-                    } else {
-                        "Please provide a valid user".to_string()
+                        println!("Error: {}", why);
                     }
-                },
-                _ => "not implemented".to_string(),
-            };
-            
-            if let Err(why) = command
-                .create_interaction_response(&ctx.http, |response| {
-                    response
-                        .kind(InteractionResponseType::ChannelMessageWithSource)
-                        .interaction_response_data(|message| message.content(content))
-                })
-                .await
-            {
-                println!("Cannot respond to slash command: {}", why);
+                }
+                _ => {
+                    println!("not implemented: {}", command.data.name.as_str().to_string());
+                }
             }
+        } else if let Interaction::MessageComponent(command) = interaction {
+            println!("MessageComponent\n command.id: {} user.id: {} data.custom_id: {}", command.id, command.user.id, command.data.custom_id);
+            if let Err(why) = command.create_interaction_response(&ctx.http, |response| {
+                response
+                    .kind(InteractionResponseType::ChannelMessageWithSource)
+                    .interaction_response_data(|message| {
+                        message.content("Button has been pressed.".to_string())
+                    })
+            })
+            .await
+            {
+                println!("Error: {}", why);
+            }
+        } else {
+            println!("We're outside in spooky");
         }
     }
 
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected", ready.user.name);
 
-        let commands = ApplicationCommand::set_global_application_commands(&ctx.http, |commands| {
+        let _ = GuildId(268239941195137025).set_application_commands(&ctx.http, |commands| {
             commands
                 .create_application_command(|command| {
-                    command.name("ping").description("A ping command")
-                })
-                .create_application_command(|command| {
-                    command.name("id").description("Get a user ID").create_option(|option| {
-                        option
-                            .name("id")
-                            .description("The user ID to lookup")
-                            .kind(ApplicationCommandOptionType::User)
-                            .required(true)
-                    })
+                    command
+                        .name("gcommand1")
+                        .description("An awesome command 1.")
                 })
                 .create_application_command(|command| {
                     command
-                        .name("welcome")
-                        .description("welcome a user")
+                        .name("gcommand2")
+                        .description("An awesome command 2.")
                         .create_option(|option| {
                             option
-                                .name("user")
-                                .description("The user to welcome")
-                                .kind(ApplicationCommandOptionType::User)
-                                .required(true)
-
-                        })
-                        .create_option(|option| {
-                            option
-                                .name("message")
-                                .description("The message to send")
+                                .name("stringopt")
+                                .description("The string to send")
                                 .kind(ApplicationCommandOptionType::String)
                                 .required(true)
                                 .add_string_choice("Welcome friend!", "friend")
                                 .add_string_choice("Do you want coffee", "coffee")
                                 .add_string_choice("Welcome to the club", "club")
                         })
+                        .create_option(|option| {
+                            option
+                                .name("opt2")
+                                .description("The string to send")
+                                .kind(ApplicationCommandOptionType::String)
+                                .required(true)
+                                .add_string_choice("This is string1", "str1")
+                                .add_string_choice("This is string2", "str2")
+                        })
                 })
         })
         .await;
-
-        println!("I now have the following commands: {:#?}", commands);
-
-        let guild_command = GuildId(268239941195137025)
-            .create_application_command(&ctx.http, |command| {
-                command.name("awesome").description("An awesome command.")
-            })
-            .await;
-            println!("I created the guild command: {:#?}", guild_command);
     }
 }
 
